bafa973d7370ebbd6057b4f9fa5cc9d5539fb810
gidskql6671 <gidskql6671@gmail.com>
2021-11-28 23:11:22 +0900

    AOP를 공부하기 전, 프록시 객체에 대해 먼저 알아보자

---
+++ src/main/java/aop/IterCalculator.java
@@ -0,0 +1,14 @@
+package aop;
+
+public class IterCalculator implements Calculator {
+
+
+    @Override
+    public long factorial(long num) {
+        long result = 1;
+        for (long i = 1; i <= num; i++) {
+            result += i;
+        }
+        return result;
+    }
+}
