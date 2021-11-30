26b8ee5d96eb9275527816c5b84b9006105b936c
gidskql6671 <gidskql6671@gmail.com>
2021-11-30 22:39:06 +0900

    @Aspect를 사용하여 간단한 캐시를 만들어봄

--- src/main/java/aop/AopMain.java
+++ src/main/java/aop/AopMain.java
@@ -3,15 +3,16 @@ package aop;
 import aop.calculator.Calculator;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class Main {
+public class AopMain {
     
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppContext.class);
 
         Calculator cal = ctx.getBean(Calculator.class);
-        long result = cal.factorial(5);
-        System.out.println("cal.factorial(5) = " + result);
-        System.out.println(cal.getClass().getName());
+        
+        cal.factorial(5);
+        cal.factorial(5);
+        cal.factorial(10);
         
         ctx.close();
     } 
---
+++ src/main/java/aop/CacheAspect.java
@@ -0,0 +1,42 @@
+package aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@Aspect
+@Component
+public class CacheAspect {
+    private final Map<Long, Object> cache = new HashMap<>();
+
+    @Pointcut("execution(public * aop.calculator..*(..))")
+    public void cacheTarget() {
+        
+    }
+    
+    @Around("cacheTarget()")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        Long num = (Long) joinPoint.getArgs()[0];
+        Signature sig = joinPoint.getSignature();
+        if (cache.containsKey(num)) {
+            System.out.printf("%s.%s(%s) 캐시에 저장된 값 사용함.\n",
+                    joinPoint.getTarget().getClass().getSimpleName(),
+                    sig.getName(), Arrays.toString(joinPoint.getArgs()));
+            return cache.get(num);
+        }
+        
+        Object result = joinPoint.proceed();
+        cache.put(num, result);
+        System.out.printf("%s.%s(%s) 캐시에 새로운 값을 저장함.\n",
+                joinPoint.getTarget().getClass().getSimpleName(),
+                sig.getName(), Arrays.toString(joinPoint.getArgs()));
+        return result;
+    }
+}
