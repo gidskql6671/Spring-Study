3a1f9609127668ae6ac3d7bebeaaf8a7c3a78cc6
gidskql6671 <gidskql6671@gmail.com>
2021-12-04 14:31:23 +0900

    JDBC 사용을 위한 DataSource 설정

---
+++ src/main/java/jdbc/config/AppContext.java
@@ -0,0 +1,31 @@
+package jdbc.config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppContext {
+
+    @Value("${datasource.driverClassName}")
+    private String driverClassName;
+    @Value("${datasource.url}")
+    private String url;
+    @Value("${datasource.username}")
+    private String username;
+    @Value("${datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        DataSource ds = new DataSource();
+        ds.setDriverClassName(driverClassName);
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setInitialSize(2);
+        ds.setMaxActive(10);
+        return ds;
+    }
+}
