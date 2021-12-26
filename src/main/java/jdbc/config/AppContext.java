bd8367311e8b43091e64dfd02406cd1f2d97c846
gidskql6671 <gidskql6671@gmail.com>
2021-12-26 19:52:55 +0900

    Spring Transactional 기능 사용

--- src/main/java/jdbc/config/AppContext.java
+++ src/main/java/jdbc/config/AppContext.java
@@ -7,8 +7,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 @PropertySource(value= "classpath:/dataSource.properties")
 @ComponentScan(basePackageClasses = JdbcApp.class)
 public class AppContext {
@@ -33,5 +37,12 @@ public class AppContext {
         ds.setMaxActive(10);
         ds.setMaxIdle(10);
         return ds;
+    }
+    
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        DataSourceTransactionManager tm = new DataSourceTransactionManager();
+        tm.setDataSource(dataSource());
+        return tm;
     }
 }
