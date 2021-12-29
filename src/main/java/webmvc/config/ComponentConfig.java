415f6aa3f67259a9d3ec0315becc69956bf4555e
gidskql6671 <gidskql6671@gmail.com>
2021-12-30 00:36:29 +0900

    파일명 변경

---
+++ src/main/java/webmvc/config/ComponentConfig.java
@@ -0,0 +1,9 @@
+package webmvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "webmvc")
+public class ComponentConfig {
+}
--- src/main/java/webmvc/config/DatabaseConfig.java
+++ src/main/java/webmvc/config/DatabaseConfig.java
@@ -2,24 +2,17 @@ package webmvc.config;
 
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.stereotype.Controller;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
 @PropertySource("classpath:/dataSource.properties")
-@ComponentScan(basePackages = "webmvc",
-        excludeFilters = {
-                @ComponentScan.Filter(
-                        type = FilterType.ANNOTATION,
-                        classes = {Controller.class, Configuration.class}
-                )
-        }
-)
-public class MemberConfig {
+public class DatabaseConfig {
     
     @Value("${datasource.driverClassName}")
     private String driverClassName;
