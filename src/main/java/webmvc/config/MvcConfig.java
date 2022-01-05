d0715fde828ac0a2ba18079349fbdad7bee9d17e
gidskql6671 <gidskql6671@gmail.com>
2022-01-06 01:31:55 +0900

    MessageSource 사용

--- src/main/java/webmvc/config/MvcConfig.java
+++ src/main/java/webmvc/config/MvcConfig.java
@@ -2,8 +2,10 @@ package webmvc.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -15,14 +17,14 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 @Configuration
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
-    
+
     private ApplicationContext applicationContext;
 
     @Autowired
     public void setApplicationContext(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
-    }    
-    
+    }
+
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         // enable()을 해줘야 defaultServletRequestHandler와 SimpleUrlHandlerMapping Bean이 만들어진다.
@@ -45,13 +47,14 @@ public class MvcConfig implements WebMvcConfigurer {
         templateResolver.setSuffix(".html");
         return templateResolver;
     }
+
     @Bean
     public SpringTemplateEngine templateEngine() {
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.setTemplateResolver(templateResolver());
         return templateEngine;
     }
-    
+
     @Bean
     public ThymeleafViewResolver viewResolver() {
         ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
@@ -59,4 +62,13 @@ public class MvcConfig implements WebMvcConfigurer {
         viewResolver.setCharacterEncoding("UTF-8");
         return viewResolver;
     }
+    
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages.message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
 }
