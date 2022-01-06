ffe2da20bbac1452caa1d1ee4753d390f2dfdd1d
gidskql6671 <gidskql6671@gmail.com>
2022-01-06 23:55:42 +0900

    컨트롤러명을 더 직관적으로 변경

--- src/main/java/webmvc/controller/SignupController.java
+++ src/main/java/webmvc/controller/SignupController.java
@@ -4,26 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import webmvc.dto.MemberSignupRequest;
 import webmvc.service.MemberSignupService;
 
 import javax.validation.Valid;
 
 @Controller
-public class MemberController {
+@RequestMapping("/signup")
+public class SignupController {
 
     private final MemberSignupService memberSignupService;
 
-    public MemberController(MemberSignupService memberSignupService) {
+    public SignupController(MemberSignupService memberSignupService) {
         this.memberSignupService = memberSignupService;
     }
 
-    @GetMapping("/signup")
+    @GetMapping
     public String signup() {
         return "member/signup";
     }
 
-    @PostMapping("/signup")
+    @PostMapping
     public String signup(@Valid MemberSignupRequest memberSignupRequest, BindingResult result) {
         try {
             memberSignupService.signup(memberSignupRequest);
