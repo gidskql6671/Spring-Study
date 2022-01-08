ed55bba27eca72877261f264c3bd0579d7bbc8e4
gidskql6671 <gidskql6671@gmail.com>
2022-01-08 21:57:00 +0900

    ResponseEntity를 사용하여 응답다루기

--- src/main/java/webmvc/controller/MemberDetailController.java
+++ src/main/java/webmvc/controller/MemberDetailController.java
@@ -1,11 +1,14 @@
 package webmvc.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import webmvc.dao.MemberDao;
 import webmvc.domain.Member;
+import webmvc.dto.ErrorResponse;
 import webmvc.dto.MemberDetailResponse;
 import webmvc.exception.MemberNotFoundException;
 
@@ -20,16 +23,26 @@ public class MemberDetailController {
 	}
 	
 	@GetMapping("/{id}")
-	public MemberDetailResponse detail(@PathVariable("id") Long memberId) {
+	public ResponseEntity<Object> detail(@PathVariable("id") Long memberId) {
 		try {
 			Member member = memberDao
 					.findById(memberId)
 					.orElseThrow(MemberNotFoundException::new);
 
-			return new MemberDetailResponse(member.getId(), member.getEmail(), member.getRegisterDateTime());
+			return ResponseEntity
+					.ok(new MemberDetailResponse(
+							member.getId(), 
+							member.getEmail(), 
+							member.getRegisterDateTime()
+					));
 		}
 		catch(RuntimeException e) {
-			throw new MemberNotFoundException();
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+					.body(new ErrorResponse("Not Found Member"));
+			
+			// body 내용이 비어있다면, build()를 통해 body 없이 보낼 수 있음.
+			// NotFound(404)나 BadRequest(400)과 같이 자주 쓰는 상태코드는 메서드로 있음.
+			// return ResponseEntity.notFound().build();
 		}
 	}
 }
