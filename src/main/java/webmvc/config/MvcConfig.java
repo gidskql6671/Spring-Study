c24b4ef1fa4c7f9d5f4ce95c0fd9fa6300655560
gidskql6671 <gidskql6671@gmail.com>
2022-01-08 21:21:59 +0900

    WebMvcConfigurer를 통해 json convert 설정을 전체적으로 적용할 수 있다.
    코드에는 date 포맷만을 설정해주었지만, 다른 설정도 많다.
    추가로, 전역으로 적용된 date format보다 개별 속성에 적용된 @JsonFormat이 우선시 된다.

--- src/main/java/webmvc/config/MvcConfig.java
+++ src/main/java/webmvc/config/MvcConfig.java
@@ -1,16 +1,22 @@
 package webmvc.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.*;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import webmvc.interceptor.AuthCheckInterceptor;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -46,6 +52,17 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(authCheckInterceptor())
                 .addPathPatterns("/member/**")
                 .addPathPatterns("/logout");
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder
+                .json()
+//                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .simpleDateFormat("yyyyMMddHHmmss")
+                .build();
+        converters.add(0,
+                new MappingJackson2HttpMessageConverter(objectMapper));
     }
 
     @Bean
--- src/main/java/webmvc/dto/MemberDetailResponse.java
+++ src/main/java/webmvc/dto/MemberDetailResponse.java
@@ -1,6 +1,5 @@
 package webmvc.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,7 +14,5 @@ import java.time.LocalDateTime;
 public class MemberDetailResponse {
 	private Long id;
 	private String email;
-//	@JsonFormat(shape = JsonFormat.Shape.STRING)
-	@JsonFormat(pattern = "yyyyMMddHHmmss")
 	private LocalDateTime registerDateTime;
 }
